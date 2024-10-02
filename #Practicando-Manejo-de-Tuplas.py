f76ddf1353a8f8cc20229e95a5d047871e17e73d
Emmanuel Castruita Soto <a.23308051281176@cbtis128.edu.mx>
2024-10-01 20:54:28 -0600

    Add files via upload

---
+++ #Practicando-Manejo-de-Tuplas.py
@@ -0,0 +1,76 @@
+#Practicando-Manejo-de-Tuplas
+
+print(" ")#imprime un espacio vacio
+
+print("Castruita Soto Emmanuel 3W")
+
+print(" ")#imprime un espacio vacio
+
+lista = ("apple", "banana", "cherry")#crea una lista tuple
+
+print(lista)#imprime lista
+
+print(" ")#imprime un espacio vacio
+
+lista = ("apple", "banana", "cherry", "apple", "cherry")#valores de lista
+
+print(lista)#imprime lista
+
+print(" ")#imprime un espacio vacio
+lista = ("apple", "banana", "cherry")#valores de lista
+
+print(len(lista))#imprime la lista
+
+print(" ")#imprime un espacio vacio
+
+lista = ("apple",)#crea una lista tuple
+
+print(type(lista))#imprime la lista de forma de tipo
+
+#NOT a tuple
+
+thistuple = ("apple")#crea una lista tuple
+
+print(type(thistuple))#imprime la lista de forma de tipos
+
+print(" ")#imprime un espacio vacio
+
+#Crear un menu con tuple
+
+thistuple = ("soda", "piza", "hamburgesa")#crea una lista tuple
+
+print(thistuple)#imprime la lista
+
+print(" ")#imprime un espacio vacio
+
+# lista de peliculas 
+
+thistuple = tuple(("jumanji 1", "jumanji 2", "spiderman 3", "cars 1", "cars 2"))#crea una lista tuple
+
+print("estas son las mejores peliculas para ver en familia:")
+
+print(thistuple)#imprime la lista
+print(" ")#imprime un espacio vacio
+
+#estos son los mejores postres 
+
+print("los mejores postres del momento:")
+
+lista = ("pastel", "dona ", "flan")#crea una lista tuple
+
+print(lista)#imprime la lista
+
+print(" ")#imprime un espacio vacio
+
+#Tupla con miembros duplicados
+
+lista = ("apple", "banana", "cherry", "apple", "cherry")#crea una lista tuple
+
+print(lista)#imprime la lista
+
+print(" ")#imprime un espacio vacio
+
+mytuple = ("manzana", "banana", "cereza")#crea mi lista tuple
+
+print(type(lista))#imprime la lista en forma de tipos
+
